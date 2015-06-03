36d54ae8c8e51f5c4d11111626705b5e4b59c462
Ruben Undheim <ruben.undheim@gmail.com>
2015-06-03 23:23:53 +0200

    Added license headers to many source code files

--- src/syntax/main.c
+++ src/syntax/main.c
@@ -1,3 +1,42 @@
+/*
+ *   Copyright (C) 1989-1991 Yale University
+ *
+ *   This work is distributed in the hope that it will be useful; you can
+ *   redistribute it and/or modify it under the terms of the
+ *   GNU General Public License as published by the Free Software Foundation;
+ *   either version 2 of the License,
+ *   or any later version, on the following conditions:
+ *
+ *   (a) YALE MAKES NO, AND EXPRESSLY DISCLAIMS
+ *   ALL, REPRESENTATIONS OR WARRANTIES THAT THE MANUFACTURE, USE, PRACTICE,
+ *   SALE OR
+ *   OTHER DISPOSAL OF THE SOFTWARE DOES NOT OR WILL NOT INFRINGE UPON ANY
+ *   PATENT OR
+ *   OTHER RIGHTS NOT VESTED IN YALE.
+ *
+ *   (b) YALE MAKES NO, AND EXPRESSLY DISCLAIMS ALL, REPRESENTATIONS AND
+ *   WARRANTIES
+ *   WHATSOEVER WITH RESPECT TO THE SOFTWARE, EITHER EXPRESS OR IMPLIED,
+ *   INCLUDING,
+ *   BUT NOT LIMITED TO, WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A
+ *   PARTICULAR
+ *   PURPOSE.
+ *
+ *   (c) LICENSEE SHALL MAKE NO STATEMENTS, REPRESENTATION OR WARRANTIES
+ *   WHATSOEVER TO
+ *   ANY THIRD PARTIES THAT ARE INCONSISTENT WITH THE DISCLAIMERS BY YALE IN
+ *   ARTICLE
+ *   (a) AND (b) above.
+ *
+ *   (d) IN NO EVENT SHALL YALE, OR ITS TRUSTEES, DIRECTORS, OFFICERS,
+ *   EMPLOYEES AND
+ *   AFFILIATES BE LIABLE FOR DAMAGES OF ANY KIND, INCLUDING ECONOMIC DAMAGE OR
+ *   INJURY TO PROPERTY AND LOST PROFITS, REGARDLESS OF WHETHER YALE SHALL BE
+ *   ADVISED, SHALL HAVE OTHER REASON TO KNOW, OR IN FACT SHALL KNOW OF THE
+ *   POSSIBILITY OF THE FOREGOING.
+ *
+ */
+
 /* ----------------------------------------------------------------- 
 FILE:	    main.c
 DESCRIPTION:This file contains main control routine for the syntax  
