99fdab84630205acfbd351be16480bab80b03d96
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2017-03-07 17:50:03 +0800

    Correcting indention

--- src/mincut/main.c
+++ src/mincut/main.c
@@ -73,97 +73,95 @@ Mincut( argc , argv )
 int argc ;
 char *argv[] ;
 {
+	char *YinitProgram(), *Ystrclone() ;
+	char filename[LRECL] ;
+	char command[LRECL] ;
+	char *ptr ;
+	int  arg_count ;
+	int  yaleIntro() ;
+	int  debug ;
+	FILE *fp ;
+	char *twdir, *Ygetenv() ;
 
-    char *YinitProgram(), *Ystrclone() ;
-    char filename[LRECL] ;
-    char command[LRECL] ;
-    char *ptr ;
-    int  arg_count ;
-    int  yaleIntro() ;
-    int  debug ;
-    FILE *fp ;
-    char *twdir, *Ygetenv() ;
+	/* start up cleanup handler */
+	YINITCLEANUP( argv[0], NULL, MAYBEDUMP ) ;
 
-    /* start up cleanup handler */
-    YINITCLEANUP( argv[0], NULL, MAYBEDUMP ) ;
+	Yinit_memsize( EXPECTEDMEMORY ) ;
 
-    Yinit_memsize( EXPECTEDMEMORY ) ;
-
-    if( argc < 2 || argc > 3 ){
-	syntax() ;
-    } else {
-	debug      = FALSE ;
-	arg_count = 1 ;
-	if( *argv[1] == '-' ){
-	    for( ptr = ++argv[1]; *ptr; ptr++ ){
-		switch( *ptr ){
-		case 'd':
-		    debug = TRUE ;
-		    break ;
-		default:
-		    sprintf( YmsgG,"Unknown option:%c\n", *ptr ) ;
-		    M(ERRMSG,"main",YmsgG);
-		    syntax() ;
-		}
-	    }
-	    YdebugMemory( debug ) ;
-	    cktNameG = Ystrclone( argv[++arg_count] );
-
-	    /* now tell the user what he picked */
-	    M(MSG,NULL,"\n\nSyntax switches:\n" ) ;
-	    if( debug ){
-		YsetDebug( TRUE ) ;
-		M(MSG,NULL,"\tdebug on\n" ) ;
-	    } 
-	    M(MSG,NULL,"\n" ) ;
-	} else if( argc == 2 ){
-	    /* order is important here */
-	    YdebugMemory( FALSE ) ;
-	    cktNameG = Ystrclone( argv[1] );
-
+	if( argc < 2 || argc > 3 ){
+		syntax() ;
 	} else {
-	    syntax() ;
+		debug      = FALSE ;
+		arg_count = 1 ;
+		if( *argv[1] == '-' ){
+			for( ptr = ++argv[1]; *ptr; ptr++ ){
+				switch( *ptr ){
+					case 'd':
+						debug = TRUE ;
+						break ;
+					default:
+						sprintf( YmsgG,"Unknown option:%c\n", *ptr ) ;
+						M(ERRMSG,"main",YmsgG);
+						syntax() ;
+				}
+			}
+			YdebugMemory( debug ) ;
+			cktNameG = Ystrclone( argv[++arg_count] );
+
+			/* now tell the user what he picked */
+			M(MSG,NULL,"\n\nSyntax switches:\n" ) ;
+			if( debug ){
+				YsetDebug( TRUE ) ;
+				M(MSG,NULL,"\tdebug on\n" ) ;
+			} 
+			M(MSG,NULL,"\n" ) ;
+		} else if( argc == 2 ){
+			/* order is important here */
+			YdebugMemory( FALSE ) ;
+			cktNameG = Ystrclone( argv[1] );
+
+		} else {
+			syntax() ;
+		}
 	}
-    }
 
-    /* we can change this value in the debugger */
-    YinitProgram(NOCUT, VERSION, yaleIntro) ;
+	/* we can change this value in the debugger */
+	YinitProgram(NOCUT, VERSION, yaleIntro) ;
 
-    if( twdir = TWFLOWDIR ){
-        sprintf(command, "awk -f %s/bin/splt_file.a %s.cel", twdir , 
-	cktNameG ) ;
-    } else {
-	printf("ERROR:TWDIR environment variable not set.\n");
-	printf("Please set it to TimberWolf root directory\n");
-	YexitPgm( PGMFAIL ) ;
-    }
+	/*if( twdir = TWFLOWDIR ){
+		sprintf(command, "awk -f %s/bin/splt_file.a %s.cel", twdir, cktNameG ) ;
+	} else {
+		printf("ERROR:TWDIR environment variable not set.\n");
+		printf("Please set it to TimberWolf root directory\n");
+		YexitPgm( PGMFAIL ) ;
+	}*/
 
-    read_par() ;
+	read_par() ;
 
-    sprintf( filename, "%s.cel", cktNameG ) ;
-    fp = TWOPEN( filename, "r", ABORT ) ;
-    readcells( fp ) ;
-    TWCLOSE( fp ) ;
+	sprintf( filename, "%s.cel", cktNameG ) ;
+	fp = TWOPEN( filename, "r", ABORT ) ;
+	readcells( fp ) ;
+	TWCLOSE( fp ) ;
 
-    sprintf( filename, "%s.mcel", cktNameG ) ;
-    fp = TWOPEN( filename, "w", ABORT ) ;
-    output( fp ) ;
-    TWCLOSE( fp ) ;
+	sprintf( filename, "%s.mcel", cktNameG ) ;
+	fp = TWOPEN( filename, "w", ABORT ) ;
+	output( fp ) ;
+	TWCLOSE( fp ) ;
 
-    sprintf( filename, "%s.stat", cktNameG ) ;
-    fp = TWOPEN( filename, "a", ABORT ) ;
-    update_stats( fp ) ;
-    TWCLOSE( fp ) ;
-    
-    printf( "Splitting %s.cel into " , cktNameG ) ;
-    printf( "%s.scel and %s.mcel...\n" , cktNameG , cktNameG ) ;
-    fflush( stdout ) ;
-    Ysystem( "Mincut", ABORT, command, NULL ) ;
-    printf( "\tdone!\n\n" ) ;
-    fflush( stdout ) ;
+	sprintf( filename, "%s.stat", cktNameG ) ;
+	fp = TWOPEN( filename, "a", ABORT ) ;
+	update_stats( fp ) ;
+	TWCLOSE( fp ) ;
+
+	printf( "Splitting %s.cel into " , cktNameG ) ;
+	printf( "%s.scel and %s.mcel...\n" , cktNameG , cktNameG ) ;
+	fflush( stdout ) ;
+	Ysystem( "Mincut", ABORT, command, NULL ) ;
+	printf( "\tdone!\n\n" ) ;
+	fflush( stdout ) ;
 
 
-    YexitPgm( PGMOK ) ;
+	YexitPgm( PGMOK ) ;
 
 } /* end main */
 
