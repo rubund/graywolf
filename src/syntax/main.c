b42dfb745a4dbe5bd1adb178107ad2c64c25b75c
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2017-03-07 17:50:29 +0800

    Changing into a library function

--- src/syntax/main.c
+++ src/syntax/main.c
@@ -66,7 +66,7 @@ static char SccsId[] = "@(#) main.c version 1.6 2/23/91" ;
 #define EXPECTEDMEMORY  (1024 * 1024)  /* 1M should be enough */
 #define VERSION         "v1.1"
 
-main( argc , argv )
+Syntax( argc , argv )
 int argc ;
 char *argv[] ;
 {
