87800076bca2fbc0ad6e882d9195034e56e172b7
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2017-03-10 12:38:14 +0800

    Remove this fucker

--- include/yalecad/system.h
+++ include/yalecad/system.h
@@ -7,24 +7,6 @@ REVISIONS:
 #ifndef YSYSTEM_H
 #define YSYSTEM_H
 
-#ifndef lint
-static char Ysystem_HId[] = "@(#) system.h version 1.1 3/5/92" ;
-#endif
-
-/*
-Ysystem -
-Function:
-    An abstract interface to the system call.  The first argument is
-    the name of the program to be executed.  It is used in the error
-    message when the system call fails for some reason.  The second
-    argument is whether to abort the program or not.  The third argument
-    is the actual command that is to be processed.  The fourth argument
-    is the function to call upon failure.  It has no arguments.
-    Ysystem return the system error code if failure; 0 otherwise.
-*/
-extern INT Ysystem( P4(char *program,BOOL abortFlag,char *exec_statement,
-    INT (*abort_func)() ) ) ;
-
 /*
 YcopyFile -
 Function:
