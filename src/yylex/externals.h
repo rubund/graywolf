9d53afc818cb2e7f6911e303300dd2065e8a2a04
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2017-03-26 15:29:53 +0800

    deleting old experiment

--- src/yylex/externals.h
+++
@@ -1,8 +0,0 @@
-extern rw_table *rwtable;
-extern int yylex(FILE *yyin,FILE *yyout);
-extern void reset_yylex(unsigned long sizeof_rwtableS, unsigned long sizeof_yycrankS, unsigned long sizeof_yysvecS);
-extern yysvf *yysvec;
-extern yywork *yycrank;
-extern unsigned long line_countS;
-extern char *yyextra;
-extern char *yytext;
