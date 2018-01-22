09aba691c92022272d9702e1b2706f26d4ddf911
Ruben Undheim <ruben.undheim@gmail.com>
2018-01-22 18:28:13 +0100

    m

--- fixes.sh
+++ fixes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git grep -l "typedef" | grep -v "fixes.sh" | while read f ; do perl -pi -e 's#^.*?typedef\s*[^ ].*?INT##' $f ; done
+git grep -l "typedef" | grep -v "fixes.sh" | while read f ; do perl -pi -e 's#^.*?typedef\s*[^ ].*?\bINT\b##' $f ; done
 
 #git grep -l "\<int\>" | while read f; do perl -pi -e 's#\bINT\b#int#' $f ; done
 
