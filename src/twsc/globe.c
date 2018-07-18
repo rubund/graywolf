d376546e5fd635f5408c48e8820449320c435c3c
Ruben Undheim <ruben.undheim@gmail.com>
2018-07-18 22:00:08 +0200

    warning

--- src/twsc/globe.c
+++ src/twsc/globe.c
@@ -114,6 +114,8 @@ void FeedAssgn(INT row );
 void row_seg_intersect(PINBOXPTR ptr1 , PINBOXPTR ptr2 , SEGBOXPTR segptr );
 void copy_workerS_field(FEED_SEG_PTR aptr, FEED_SEG_PTR bptr );
 void assgn_impin(IPBOXPTR imptr , FEED_SEG_PTR fsptr , int row );
+void relax_padPins_pinloc();
+void relax_unequiv_pinloc();
 
 /* static variables */
 static INT *wk_headS , max_feed_in_a_rowS ;
