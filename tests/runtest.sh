6b5f72804be04f0d03c8871dab44315169acb0e2
Ruben Undheim <ruben.undheim@gmail.com>
2018-07-19 19:14:47 +0200

    Some testing now working

--- tests/runtest.sh
+++ tests/runtest.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-BINDIR=$1
-TESTNAME=$2
+SOURCEDIR=$1
+BINDIR=$2
+TESTNAME=$3
+
+#TWDIR=${BINDIR}/micro_env ${BINDIR}/src/twflow/graywolf
+TMPDIR=`mktemp -d`
+cp -r ${SOURCEDIR}/tests/${TESTNAME} ${TMPDIR}/
+
+pushd ${TMPDIR}/${TESTNAME}
+TWDIR=${BINDIR}/micro_env ${BINDIR}/src/twflow/graywolf -n ${TESTNAME}
+
+
+diff ${TESTNAME}.pl1 expected/${TESTNAME}.pl1
+RET=$?
+
+popd
+rm -rf ${TMPDIR}
+
+exit $RET
