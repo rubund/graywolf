ccb74b830d9f83f988f49330579a3d378c8b3098
Ruben Undheim <ruben.undheim@gmail.com>
2018-07-19 19:01:43 +0200

    All installed

--- tests/install_in_tmp.sh
+++ tests/install_in_tmp.sh
@@ -10,3 +10,6 @@ cp $2/src/genrows/genrows $2/micro_env/bin/
 cp $2/src/mc_compact/mc_compact $2/micro_env/bin/
 cp $2/src/syntax/syntax $2/micro_env/bin/
 cp $2/script/show_flows $2/micro_env/bin/
+cp $1/script/splt_file.a $2/micro_env/bin/
+cp -r $1/flow $2/micro_env/bin/
+ln -sf flow.noroute $2/micro_env/bin/flow/flow
---
+++ tests/runtest.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+BINDIR=$1
+TESTNAME=$2
