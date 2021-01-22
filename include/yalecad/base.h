b0692b44dfc21ded1ad61c8a2e47d262d14ebabe
Kevin Zheng <kevinz5000@gmail.com>
2021-01-22 14:52:58 -0800

    Fix build on FreeBSD

--- include/yalecad/base.h
+++ include/yalecad/base.h
@@ -161,7 +161,7 @@ typedef long SHORT_LONG ;
 
 #endif /* ultrix, linux */
 
-#if defined(THINK_C) || defined(linux)
+#if defined(THINK_C) || defined(linux) || defined(__FreeBSD__)
 #define PROTOTYPES_OK
 #endif /* Mac, linux */
 
